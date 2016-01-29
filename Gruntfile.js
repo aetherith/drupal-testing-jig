d5ddf7bc6bf7db804067022f3bbf2a38c2c28b02
Thomas C. Foulds <tfoulds@sapient.com>
2016-01-29 12:25:55 -0500

    Added base files and updated README

---
+++ Gruntfile.js
@@ -0,0 +1,137 @@
+module.exports = function (grunt) {
+    'use strict';
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * PHP
+         */
+        // Linting - Standards and Best Practices
+        phpcs: {
+            standard: {
+                src: [
+                    '**/*.php',
+                    '**/*.module',
+                    '**/*.inc',
+                    '**/*.install',
+                    '**/*.test',
+                    '**/*.profile',
+                    '**/*.theme',
+                    '**/*.js',
+                    '**/*.css',
+                    '**/*.info',
+                    '**/*.txt',
+                    '**/*.md'
+                ],
+                options: {
+                    standard: 'Drupal'
+                }
+            },
+            practice: {
+                src: ['<%= phpcs.standard.src %>'],
+                options: {
+                    standard: 'DrupalPractice'
+                }
+            }
+        },
+
+        // Beautifying
+        phpcbf: {
+            standard: {
+                src: ['<= phpcs.standard.src %>'],
+                options: {
+                    standard: 'Drupal'
+                }
+            }
+        },
+
+        /**
+         * JavaScript
+         */
+        // Linting
+        eslint: {
+            target: ['**/*.js'],
+            options: {
+                configFile: '.eslintrc'
+            }
+        },
+
+        //Beautifying
+        jsbeautifier: {
+            modify: {
+                src: ['<%= eslint.target %>'],
+                options: {
+                    config: '.jsbeautifyrc'
+                }
+            },
+            verify: {
+                src: ['<%= eslint.target %>'],
+                options: {
+                    config: '.jsbeautifyrc',
+                    mode: 'VERIFY_ONLY'
+                }
+            }
+        },
+
+        /**
+         * CSS
+         */
+        // Linting
+        csslint: {
+            src: ['**/*.css'],
+            options: {
+                csslintrc: '.csslintrc'
+            }
+        },
+
+        // Beautifying
+        // This version is not suitable for situations where individual CSS
+        // files must be kept separate from each other. You should adapt
+        // the 'files' targets to generate the correct files for your situation.
+        csscomb: {
+            options: {
+                config: '.csscombrc'
+            },
+            application: {
+                files: {
+                    'css/main.css': ['<%= csslint.src %>']
+                }
+            }
+        },
+
+        /**
+         * Parallel Task Groups
+         */
+        concurrent: {
+            phpVerify: [
+                'phpcs:standard',
+                'phpcs:practice'
+            ],
+            jsVerify: [
+               'eslint',
+               'jsbeautifier:verify'
+            ],
+            'cssVerify': [
+                'csslint'
+            ],
+        },
+
+        /**
+         * Execution Tasks
+         */
+        grunt.registerTask('verify', [
+            'concurrent:phpVerify',
+            'concurrent:jsVerify',
+            'concurrent:cssVerify'
+        ]);
+
+        grunt.registerTask('clean', [
+            'phpcbf:standard',
+            'jsbeautifier:modify',
+            'csscomb'
+        ]);
+    });
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# drupal-testing-jig
-A default set of Grunt plugins and configuration for code quality tests in Drupal
+# Grunt Drupal Testing Jig
+While building Drupal modules it's useful to have a series of tests and cleanup
+tools at your disposal to prevent code smell. I'm sure there's an official
+version out there somewhere but a few hours on Google didn't give me an easy
+bake solution so I created my own.
+
+## Dependencies
+You'll need [Node.js][node] and [Grunt][grunt] along with [phpcs][drupal-phpcs]
+configured to use the Drupal coding standards.
+
+## Use
+The modules are grouped roughly by language and the targets are defined in the
+first plugin for the language. Running the test battery is as easy as running
+`grunt verify` and running the autofix/beautifiers is `grunt clean`.
+
+[node]: https://nodejs.org/
+[grunt]: http://gruntjs.com/
+[drupal-phpcs]: https://www.drupal.org/node/1587138
+[drupal-eslint]: https://www.drupal.org/node/1955232
+[drupal-csscomb]: https://www.drupal.org/node/2399303
+[drupal-csslint]: https://www.drupal.org/node/2222049
+[drupal-csslint-repo]: http://cgit.drupalcode.org/drupal/tree/.csslintrc
+[drupal-eslint-repo]: http://cgit.drupalcode.org/drupal/tree/.eslint
